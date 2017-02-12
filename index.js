befcff22c393a783e176509bbd6a4b15504ebc2a
Nate Westheimer <nate.westheimer@gmail.com>
2017-02-11 22:08:12 -0500

    wake lock

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(cookieParser());
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+// serve static files
+app.use(express.static(__dirname + '/public'));
 
 // setup Runkeeper
 var runkeeperAccessToken;
